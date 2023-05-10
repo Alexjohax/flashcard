f806dd834c4d5ea5f854c87dd1a6425c23397c8a
Alejohax <alexander.johanssonn@gmail.com>
2023-05-10 08:35:55 +0200

    Setting up ui & api, added decks endpoints

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,29 @@
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
-const app = express();
+import Deck from "./models/Deck";
 
-const db = mongoose.connect(
-  "mongodb+srv://flashcard:pN0NSaOf0CJJuTvT@cluster0.0egfvi2.mongodb.net/?retryWrites=true&w=majority"
-);
+const PORT = 5000;
+
+const app = express();
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("gg");
 });
 
-app.get("/hello", (req: Request, res: Response) => {
-  res.send("hello world");
+app.post("/decks", async (req: Request, res: Response) => {
+  const newDeck = new Deck({
+    title: req.body.title,
+  });
+  const createdDeck = await newDeck.save();
+  res.json(createdDeck);
 });
 
-app.listen(5000);
+const db = mongoose
+  .connect(
+    "mongodb+srv://flashcard:pN0NSaOf0CJJuTvT@cluster0.0egfvi2.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log(`listening on port ${PORT}`);
+    app.listen(5000);
+  });
