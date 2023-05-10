42eee363e92b77e4a4a3f0050029af9fa50ed8ef
Alejohax <alexander.johanssonn@gmail.com>
2023-05-10 10:32:15 +0200

    Ui progress

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,16 +3,27 @@ config();
 
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
-
+import cors from "cors";
 import Deck from "./models/Deck";
 
 const PORT = 5000;
 
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("gg");
+});
+
+app.get("/decks", async (req: Request, res: Response) => {
+  //TODO: fetch decks and return to user
+  const decks = await Deck.find();
+  res.json(decks);
 });
 
 app.post("/decks", async (req: Request, res: Response) => {
